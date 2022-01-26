fe368099d05eb3e327eca6599a7ef1bf48c180bc
lazySia <tldk9640@naver.com>
2022-01-26 17:36:05 +0900

    ts_start

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,7 @@
-import styled, { keyframes } from "styled-components";
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const rotationAnimation = keyframes`
-  0% { 
-    transform: rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius:100px;
-  }
-  100% {
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
-`;
+import styled from "styled-components";
 
 function App() {
-  return (
-    <Wrapper>
-      <Title>hello</Title>
-    </Wrapper>
-  );
+  return <div />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,9 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
