bb7c35398c62bc5d37c60f4163e013e065e0771f
lazySia <tldk9640@naver.com>
2022-01-27 21:51:16 +0900

    coin tracker2

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#487eb0",
+  // #44bd32
 };
