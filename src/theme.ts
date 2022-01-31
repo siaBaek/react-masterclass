1ead592eee6d2104cfe2366a531b70a50b61fdac
lazySia <tldk9640@naver.com>
2022-01-31 23:04:16 +0900

    Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   bgColor: "#2f3640",
   textColor: "#f5f6fa",
-  accentColor: "#fbc531",
-  // #44bd32
+  accentColor: "#44bd32",
 };
