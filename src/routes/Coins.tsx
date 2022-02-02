d3a186269e925fdad5152ebadaa4f6bbe5528114
lazySia <tldk9640@naver.com>
2022-02-02 20:59:44 +0900

    cointracker

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { fetchCoins } from "../api";
+import { isDarkAtom } from "../atoms";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -75,11 +77,14 @@ interface ICoin {
 interface ICoinsProps {}
 
 function Coins() {
+  const setDarkAtom = useSetRecoilState(isDarkAtom);
+  const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
   const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
   return (
     <Container>
       <Helmet>
-        <title>코인</title>
+        <Title>코인</Title>
+        <button onClick={toggleDarkAtom}>Toggle Mode</button>
       </Helmet>
       <Header>
         <Title>Top 100 Crypto Coins</Title>
